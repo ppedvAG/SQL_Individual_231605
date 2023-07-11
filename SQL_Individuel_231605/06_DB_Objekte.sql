528d9fb49dee69873846e877155673b1814e5440
Rauchendraser <andreasr@ppedv.de>
2023-07-11 15:02:50 +0200

    DB Objekte

--- SQL_Individuel_231605/06_DB_Objekte.sql
+++ SQL_Individuel_231605/06_DB_Objekte.sql
@@ -1,4 +1,20 @@
 /*
+
+--a) adhoc Abfrage  select .. tabelle
+--b) View           select ...view
+--c) Proc            exec proc 10,5
+--d) F()            select ...f(wert) 
+
+/*
+langsam --- schnell--->
+
+b   d   a   c
+
+(c)  ....... d .....a|b........c
+
+----------------------------
+
+
 DB Design
 
 Normalisierung
@@ -60,6 +76,58 @@ select Companyname from customers c inner join orders o on c.CustomerID=o.Custom
 select * from (select * from customers ) c
 
 
+--Tabellen 
+
+
+Sicht:
+hat keine Daten.. gemerkte Abfrage
+
+select * from (select * from ...) t
+
+create view t
+as
+select ....
+
+auch INS UP DEL möglich 
+
+Warum Sichten: 
+--eig Rechte auf Sichten
+--komplexe Abfragen wiederverwendbar machen
+
+--immer with schemabinding
+
+*/
+
+drop view v1
+drop table slf
+
+create table slf (id int, stadt int, land int)
+
+insert into slf
+select 1,10,100
+UNION ALL
+select 2,20,200
+UNION ALL
+select 3,30,300
+
+
+create view v1 with schemabinding
+as
+select id, stadt , land from dbo.slf
+
+select * from v1
+select * from slf
+
+alter table slf add fluss int
+
+update slf set fluss = Id*1000
+
+select * from slf
+select * from v1
+
+alter table slf drop column land
+
+
 
 --Prozeduren
 -
@@ -98,6 +166,34 @@ select f(Sp) , f(wert) from f(Wert) where f(sp) > f(wert)
 
 where famname = 'HOPFGARTNER' ---Indizes
 where len(famname) = 11
+
+
+--Functions
+use northwind
+
+select * from [Order Details]
+
+select sum(unitprice*quantity) from [Order Details]
+where orderid = 10250
+
+create function fRngSumme(@oid int) returns money
+as
+Begin
+	return(
+			select sum(unitprice*quantity) from [Order Details]
+			where orderid = @oid
+			)
+end
+
+select dbo.fRngSumme(10250)
+
+
+select * from orders where dbo.fRngsumme(orderid) < 1000
+
+
+alter table orders add RngSumme as dbo.fRngSumme(orderid)
+
+select * from orders where rngsumme < 1000
 
 
 
