958f3d9240ac4fd2298a27b090d46fbf0b68824b
Rauchendraser <andreasr@ppedv.de>
2023-07-12 10:14:34 +0200

    Spieltabelle

---
+++ SQL_Individuel_231605/Spieltabelle.sql
@@ -0,0 +1,13 @@
+SELECT        Customers.CustomerID, Customers.CompanyName, Customers.ContactName, Customers.ContactTitle, Customers.City, Customers.Country, Orders.EmployeeID, Orders.OrderDate, Orders.Freight, Orders.ShipCountry, 
+                         Orders.ShipCity, Employees.LastName, Employees.FirstName, [Order Details].OrderID, [Order Details].ProductID, [Order Details].UnitPrice, [Order Details].Quantity, Products.ProductName, Products.UnitsInStock
+INTO KU
+FROM            Customers INNER JOIN
+                         Orders ON Customers.CustomerID = Orders.CustomerID INNER JOIN
+                         Employees ON Orders.EmployeeID = Employees.EmployeeID INNER JOIN
+                         [Order Details] ON Orders.OrderID = [Order Details].OrderID INNER JOIN
+                         Products ON [Order Details].ProductID = Products.ProductID
+
+
+insert into ku
+select * from ku
+GO 9
